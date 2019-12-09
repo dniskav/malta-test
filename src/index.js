fa1e932e7ec6e2c80044bd5adec9d20b823f880b
Dniskav <dniskav@pop-os.localdomain>
2019-12-09 15:06:02 -0500

    first steps

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'antd/dist/antd.css';
+
+import App from './components/App';
+
+const app = document.getElementById('app');
+
+render(<App />, app);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    context: __dirname,
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                use: [
+                    'babel-loader',
+                ],
+                include: path.join(__dirname, 'src'),
+                exclude: '/node_modules',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        publicPath: '../',
+                      },
+                    },
+                    'css-loader',
+                  ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css', 
+        }),
+    ],
+    devServer: {
+        port: 3001,
+        open: true,
+    }
+};
